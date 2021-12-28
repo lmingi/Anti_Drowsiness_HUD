c9c409f91dd51bb101023b016ed4184923dedca6
lmingi <mingilee98@gmail.com>
2021-12-28 15:52:54 -0600

    add intro slider

--- app/src/main/java/com/example/anti_drowsiness_hud/SplashActivity.java
+++ app/src/main/java/com/example/anti_drowsiness_hud/SplashActivity.java
@@ -5,9 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-import com.example.anti_drowsiness_hud.MainActivity;
-import com.example.anti_drowsiness_hud.R;
-
 public class SplashActivity extends Activity {
 
     Handler handler;
@@ -20,7 +17,7 @@ public class SplashActivity extends Activity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(SplashActivity.this, MainActivity.class);
+                Intent intent=new Intent(SplashActivity.this, IntroSlider.class);
                 startActivity(intent);
                 finish();
             }
