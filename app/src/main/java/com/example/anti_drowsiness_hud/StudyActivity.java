7eeebc17b9c2ef7919ef946461b00129bb862821
lmingi <mingilee98@gmail.com>
2021-12-28 16:29:06 -0600

    add imageviews to front page

--- app/src/main/java/com/example/anti_drowsiness_hud/StudyActivity.java
+++ app/src/main/java/com/example/anti_drowsiness_hud/StudyActivity.java
@@ -1,15 +1,13 @@
 package com.example.anti_drowsiness_hud;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class ActivityHome extends AppCompatActivity {
+public class StudyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        
+        setContentView(R.layout.activity_study);
     }
 }
